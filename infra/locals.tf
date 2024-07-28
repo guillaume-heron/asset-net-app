4c0d71467af25ccbc84eb60e04bd454a567ccebd
Guillaume <guillaume-heron@outlook.fr>
2024-07-28 16:49:00 +0200

    Feature 003: Structuring terraform files

---
+++ infra/locals.tf
@@ -0,0 +1,3 @@
+locals {
+  resource_group_name = "rg-${var.app_name}-${var.env}-${var.app_location}"
+}
--- infra/main.tf
+++ infra/main.tf
@@ -1,21 +1,3 @@
-terraform {
-  backend "azurerm" {}
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.113"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
-locals {
-  resource_group_name = "rg-${var.app_name}-${var.env}-${var.app_location}"
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = local.resource_group_name
   location = var.app_location
