e6b732b02c0fc82e8918b792ab75ea4f421c901a
Guillaume <guillaume-heron@outlook.fr>
2024-07-28 13:06:46 +0200

    Feature 002: Format main.tf file

--- infra/main.tf
+++ infra/main.tf
@@ -1,14 +1,14 @@
 terraform {
   backend "azurerm" {
-    resource_group_name     = "rg-${var.app_name}-shared-${var.app_location}"
-    storage_account_name    = "st${var.app_name}tfstates"
-    container_name          = "tfstates"
-    key                     = "${var.app_name}.${var.env}.tfstate"
+    resource_group_name  = "rg-${var.app_name}-shared-${var.app_location}"
+    storage_account_name = "st${var.app_name}tfstates"
+    container_name       = "tfstates"
+    key                  = "${var.app_name}.${var.env}.tfstate"
   }
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "~> 3.113"
+      source  = "hashicorp/azurerm"
+      version = "~> 3.113"
     }
   }
 }
@@ -18,6 +18,6 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name = "rg-${var.app_name}-${var.env}-${var.app_location}"
-  location = "${var.app_location}"
+  name     = "rg-${var.app_name}-${var.env}-${var.app_location}"
+  location = var.app_location
 }
