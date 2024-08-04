b7e630d0523993e9727ab42b1816596a52dec79d
Guillaume <guillaume-heron@outlook.fr>
2024-08-04 19:21:53 +0200

    Feature 006: Use tfvars file with terraform plan/apply command

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,14 +1,17 @@
 variable "env" {
   description = "The environment tag used for all resources."
+  type        = string
   default     = "dev"
 }
 
 variable "app_name" {
   description = "The name of the app."
+  type        = string
   default     = "netasset"
 }
 
 variable "app_location" {
   description = "The location of the app."
+  type        = string
   default     = "francecentral"
 }
