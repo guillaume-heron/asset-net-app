b2812ef48dc9fdfb588f059f7c830fd759d30b08
Guillaume <guillaume-heron@outlook.fr>
2024-08-19 15:59:44 +0200

    Feature 011: Create dedicated RG for Container Registry

--- infra/locals.tf
+++ infra/locals.tf
@@ -1,6 +1,7 @@
 locals {
+  resource_group_name_acr     = "rg-registry-shared-${var.app_location}"
+  container_registry_name     = "acrhubshared${var.app_location}"
   resource_group_name         = "rg-${var.app_name}-${var.env}-${var.app_location}"
   environment                 = var.env
-  container_registry_name     = "acr${var.app_name}${var.env}${var.app_location}"
   user_assigned_identity_name = "uai-acr-${var.app_name}-${var.env}-${var.app_location}"
 }
