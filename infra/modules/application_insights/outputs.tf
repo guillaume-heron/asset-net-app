6d1f1d1d94359a9dd80dda020feefad5a9efeefd
Guillaume <guillaume-heron@outlook.fr>
2024-08-20 18:08:42 +0200

    Feature 012: Update App Service Monitoring configuration

--- infra/modules/application_insights/outputs.tf
+++ infra/modules/application_insights/outputs.tf
@@ -1,3 +1,3 @@
-output "instrumentation_key" {
-  value = azurerm_application_insights.appi.instrumentation_key
+output "connection_string" {
+  value = azurerm_application_insights.appi.connection_string
 }
