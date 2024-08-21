7699b375475d7d3cf95689d70ef201c2e367e74c
Guillaume <guillaume-heron@outlook.fr>
2024-08-21 18:29:04 +0200

    Feature 013: add keyvault - update infra

--- infra/modules/container_registry/main.tf
+++ infra/modules/container_registry/main.tf
@@ -1,12 +1,12 @@
 # Create the dedicated Resource Group for Azure Container Registry
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-registry-shared-${var.location}"
+  name     = "rg-registry-shared-${var.location_short}"
   location = var.location
 }
 
 # Create an Azure Container Registry
 resource "azurerm_container_registry" "acr" {
-  name                = "acrhubshared${var.location}"
+  name                = "acrhubshared${var.location_short}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Standard"
--- infra/modules/container_registry/variables.tf
+++ infra/modules/container_registry/variables.tf
@@ -1,3 +1,7 @@
 variable "location" {
   description = "The region where the container registry and his resource group will be deployed"
 }
+
+variable "location_short" {
+  description = "The short name of the region to write on the resource name."
+}
